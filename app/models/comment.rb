a97883a6b294152077a403fdd26c3b1db07e7251
= <zackkollar@gmail.com>
2013-08-31 11:24:59 +0000

    Add proper inherentence to Comment model.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,5 @@
 class Comment < ActiveRecord::Base
+  belongs_to :post
   validates_presence_of :post_id, :bit_id, :body
   def replies
     @replies = Comment.where({:reply => true, :post_id => self.post_id})
